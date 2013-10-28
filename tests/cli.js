dfd95df6726550476fe746e55f8caff7016e5e09
Bulkan Evcimen <bulkan@gmail.com>
2013-10-28 23:02:52 +1100

    test that no input triggers help

--- tests/cli.js
+++ tests/cli.js
@@ -1,8 +1,17 @@
-var newCommand = require('../lib/commands/new').newCommand;
+var child_process = require('child_process')
+  , should        = require('chai').should()
+  , sinon         = require('sinon')
+  , path = require('path');
 
-describe('default zap interface', function(){
-  it('new', function(done) {
-    newCommand();
-    done();
-  })
+
+var zapt = path.normalize(path.resolve(path.join(__dirname, '../bin/zapt')));
+
+describe('default zat interface', function(){
+  it('no arguments should show help', function(done){
+    child_process.exec(zapt, function(error, stdout, stderr){
+      if (error) return done(error);
+      stdout.should.match(/Usage/);
+      done();
+    });
+  });
 });
