240294e3fb3f15f653461f9b8990b49b4862598d
Bulkan Evcimen <bulkan@gmail.com>
2013-10-29 09:11:20 +1100

    copies app_template to given app_path

--- tests/unit.js
+++ tests/unit.js
@@ -2,10 +2,15 @@ var fs         = require('fs')
   , should     = require('chai').should()
   , sinon      = require('sinon')
   , path       = require('path')
+  , rimraf     = require('rimraf')
   , newCommand = require('../lib/commands/new').newCommand;
 
 
 describe('new', function(){
+  after(function(done){
+    rimraf('/tmp/test_app', done);
+  });
+
   it('command copies app_template to destination', function(done){
     newCommand({
       author_name: 'Bulkan Evcimen',
@@ -15,6 +20,8 @@ describe('new', function(){
     });
 
     fs.existsSync('/tmp/test_app').should.be.ok;
+    fs.existsSync('/tmp/test_app/app.js').should.be.ok;
+    fs.existsSync('/tmp/test_app/app.css').should.be.ok;
     done();
   });
 });
