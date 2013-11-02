234080621674a8a49c5a8def254896b8d93c4f3b
Bulkan Evcimen <bulkan@gmail.com>
2013-11-03 04:22:54 +1100

    more tests

--- tests/unit.js
+++ tests/unit.js
@@ -1,15 +1,25 @@
 var fs         = require('fs')
+  , async      = require('async')
   , should     = require('chai').should()
   , sinon      = require('sinon')
   , path       = require('path')
+  , mkdirp     = require('mkdirp')
   , rimraf     = require('rimraf')
   , newCommand = require('../lib/commands/new').newCommand
   , readified_js = require('../lib/commands/server').readified_js;
 
 
 describe('new', function(){
+  
   after(function(done){
-    rimraf('/tmp/test_app', done);
+    async.parallel([
+      function(cb) {
+        rimraf('/tmp/test_app', cb);
+      },
+      function(cb){
+        rimraf('/tmp/zapt', cb);
+      }
+    ], done);
   });
 
 
@@ -24,6 +34,25 @@ describe('new', function(){
     fs.existsSync('/tmp/test_app').should.be.ok;
     fs.existsSync('/tmp/test_app/app.js').should.be.ok;
     fs.existsSync('/tmp/test_app/app.css').should.be.ok;
+    done();
+  });
+
+
+  it('command uses creates files into cwd', function(done){
+    var cwd = process.cwd();
+    mkdirp.sync('/tmp/zapt');
+    process.chdir('/tmp/zapt');
+
+    newCommand({
+      author_name: 'Bulkan Evcimen',
+      author_email: 'bulkan@gmail.com',
+      app_name: 'test app',
+      app_path: '.'
+    });
+
+    fs.existsSync('/tmp/test_app/app.js').should.be.ok;
+    fs.existsSync('/tmp/test_app/app.css').should.be.ok;
+    process.chdir(cwd);
     done();
   });
 
@@ -52,4 +81,13 @@ describe('server', function() {
     appjs.should.be.ok;
     done();
   })
+
+  it('readified_js has values from settings', function(done){
+    var appjs = readified_js(conf.app_path);
+    appjs.should.be.ok;
+    appjs.should.match(/name: "Bulkan Evcimen"/);
+    appjs.should.match(/email: "bulkan@gmail.com"/);
+    done();
+  
+  });
 });
